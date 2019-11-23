a8d8ea5d849e4097763089f8fcd716729f689f98
Pedro Gómez <pedrodeveloper14@gmail.com>
2019-11-23 11:42:21 -0600

    working with docker

--- config/mongodb.js
+++ config/mongodb.js
@@ -3,22 +3,26 @@ const Mongoose = require("mongoose");
 let database = "RegisterDB";
 let port = "27017";
 let host = "mongodb";
-let uri = `mongodb://${host}:${port}/${database}`
+let uri = `mongodb://${host}:${port}/${database}`;
 
-const connect = ()=>{
-    Mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(()=>{
-        console.log(`Conection to ${database} successfully`);
+const retry = () => {
+  return connect();
+};
+
+const connect = () => {
+  Mongoose.connect(uri, { useNewUrlParser: true })
+    .then(() => {
+      console.log(`Conection to ${database} successfully`);
     })
-    .catch((e)=>{
-        console.log(`An error happened trying to connect ${database} database`, e);
-    });;
+    .catch(error => {
+      console.log(`An error happened trying to connect ${database} database`);
+      console.log(error);
+      setTimeout(retry, 5000);
+    });
 
-    Mongoose.Promise = global.Promise;
-}
-
-
+  Mongoose.Promise = global.Promise;
+};
 
 module.exports = {
-    connect,
+  connect
 };
